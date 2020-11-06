84fd0b0b0a9f1823cb3db3ee2708dc884e55f328
Emmanuel Okon <emmaudo27@gmail.com>
2020-11-06 14:18:16 +0100

    with preloader

--- js/style.js
+++ js/style.js
@@ -1,15 +1,26 @@
-(function ($) {
-  "use strict";
+// (function ($) {
+//   "use strict";
 
-  $(window).on('load', function () {
-    var preloaderFadeOutTime = 500;
-    function hidePreloader() {
-      var preloader = $('.spinner-wrapper');
-      setTimeout(function () {
-        preloader.fadeOut(preloaderFadeOutTime);
-      }, 500);
-    }
-    hidePreloader();
-  });
+//   $(window).on('load', function () {
+//     var preloaderFadeOutTime = 500;
+//     function hidePreloader() {
+//       var preloader = $('.spinner-wrapper');
+//       setTimeout(function () {
+//         preloader.fadeOut(preloaderFadeOutTime);
+//       }, 500);
+//     }
+//     hidePreloader();
+//   });
+// }
+// )
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 1500);
 }
-)
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
