46c135bfd5eea95dbe207431e77a0622a75626ac
Emmanuel Okon <emmaudo27@gmail.com>
2021-11-09 13:05:22 +0100

    trusted logos

--- js/style.js
+++ js/style.js
@@ -15,11 +15,53 @@ window.onscroll = () => {
 // Hamburger Menu ends
 
 window.onscroll = () => {
-    navbar.classList.remove('active');
-  
-    if (window.scrollY > 50) {
-      document.querySelector('.header').classList.add('active');
-    } else {
-      document.querySelector('.header').classList.remove('active');
-    }
-  };
+  navbar.classList.remove('active');
+
+  if (window.scrollY > 50) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+
+  if (window.scrollY > 60) {
+    document.querySelector('#scroll-top').classList.add('active');
+  } else {
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+};
+
+// window.onscroll = () =>{
+
+//   menu.classList.remove('fa-times');
+//   navbar.classList.remove('active');
+
+//   if(window.scrollY > 60){
+//     document.querySelector('#scroll-top').classList.add('active');
+//   }else{
+//     document.querySelector('#scroll-top').classList.remove('active');
+//   }
+
+// }
+
+var swiper = new Swiper('.trusted-slider', {
+  spaceBetween: 20,
+  loop: true,
+
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true,
+  },
+  grabCursor: false,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    991: {
+      slidesPerView: 5,
+    },
+  },
+});
