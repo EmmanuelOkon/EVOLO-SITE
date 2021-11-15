2eb8aa77efec10907ecd587c8ab2229830a55610
Emmanuel Okon <emmaudo27@gmail.com>
2021-11-15 05:29:27 +0100

    scroll spy

--- js/style.js
+++ js/style.js
@@ -14,6 +14,29 @@ window.onscroll = () => {
 
 // Hamburger Menu ends
 
+// scroll spy
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.header .navbar a');
+
+section.forEach(sec => {
+  let top = window.scrollY;
+  let offset = sec.offsetTop - 200;
+  let height = sec.offsetHeight;
+  let id = sec.getAttribute('id');
+
+  if (top >= offset && top < offset + height) {
+    navLinks.forEach(link => {
+      link.classList.remove('active');
+      document
+        .querySelector('.header .navbar a[href*=' + id + ']')
+        .classList.add('active');
+    });
+  }
+});
+
+
+
+
 window.onscroll = () => {
   navbar.classList.remove('active');
 
@@ -92,6 +115,17 @@ var swiper = new Swiper('.test', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+
+
+
+
+
+
+
+
+
+
 
 
 const form = document.getElementById('formInput');
